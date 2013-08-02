374ddead4b0b4c3c28c6a34dc5b087c6a9ae99ab
Olga Brani <olgabrani@grnet.gr>
2013-08-02 13:30:02 +0300

    new ui: new functionalities in vm list page
    - Selected VM checkbox is visible after VM mouseleave
    - VM actions are enabled according to selected VMs (running,stopped,
      both, single)
    - Modal window for VM connect
    - VM main actions are moved from header to main area

--- snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common.js
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common.js
@@ -22,6 +22,77 @@ ui.setSidebarHeight = function(){
 	$('.lt-sidebar').height(ltSidebarHeight);
 }
 
+ui.cntCheckbox = function(){ 
+	var all = $('.options .checkbox-checked').length;
+	var running = $('.options .checkbox-checked').parents('.container').find('.img.running').length;
+	var stopped = $('.options .checkbox-checked').parents('.container').find('.img.stopped').length;
+    $('.header .main-actions li a').removeClass('active');  
+    if ( (running*stopped) > 0 ){          
+         $('.header .main-actions li.both a').addClass('active');   
+         $('.header .main-actions li.single a').removeClass('active');   
+    } else {
+        if (running > 0) {
+         	$('.header .main-actions li.both a').addClass('active');   
+         	$('.header .main-actions li.running a').addClass('active');   
+        } else if (stopped>0) {
+        	$('.header .main-actions li.both a').addClass('active');   
+        	$('.header .main-actions li.stopped a').addClass('active');   
+
+        } else {
+
+        }
+        if ( all > 1 ) {
+        	$('.header .main-actions li.single a').removeClass('active');   
+        }
+
+    }
+}
+ui.VMactionsInit = function(){
+
+	// if VM is stopped hide connect option 
+	$('.vms li .container .stopped').parents('.container').find('.options .connect').hide();
+
+	var vm_container = $('.vms li .container');
+	var vm_img = vm_container.find('.img-wrap');
+
+	vm_img.mouseenter(
+      function (e) {
+        $(this).parents('.container').find('.options').slideDown();
+      }
+    );
+
+    $('li .container .options').mouseleave(
+        function(e){
+        	var checkbox = $(this).find('.custom_checkbox');
+
+	        if (checkbox.hasClass('checkbox-checked')){
+	            checkbox.parents('.container').find('.more_checkbox').html(checkbox.clone());
+	            
+	        } else {
+	        	 checkbox.parents('.container').find('.more_checkbox').html('');
+	        }
+            $('li .container .options').hide();
+            
+            
+        }
+    );
+
+    $('li .container .check').click(function(e){
+        e.preventDefault();
+        var checkbox = $(this).find('.custom_checkbox');
+
+        checkbox.toggleClass('checkbox-checked');
+        
+        if(checkbox.hasClass('checkbox-checked')){
+            checkbox.html('b');
+        }
+        else{
+            checkbox.html('a'); 
+      
+        }
+        ui.cntCheckbox();
+    })
+}
 
 
 $(document).ready(function(){
@@ -55,8 +126,12 @@ $(document).ready(function(){
 		e.preventDefault();
 	})
 
-	
-
+	ui.VMactionsInit();
+	$('.main-actions li a').click(function(e){
+		if (!($(this).hasClass('active'))) {
+			e.preventDefault();
+		}
+	})
 
 })
 
--- snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common_ab.js
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common_ab.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
     //show selection window of vm_list_v1.html
+
+    
+
+
+
+
+
+/*
     var vm_options_win;
-    $('li .vm_container').mouseenter(function(event){
+    $('li .container .img').mouseenter(function(event){
         event.preventDefault();
-        vm_options_win = $(this).find('.vm_options');
+        var container = $(this).parents('.container');
+        container.css('border-color','grey');
+        vm_options_win = $(this).parents('.vm_container').find('.vm_options');
         if(vm_options_win.length!==0){
             vm_options_win.removeClass('invisible');
             vm_options_win.slideDown('slow');
@@ -12,15 +22,15 @@ $(document).ready(function(){
 
     }).mouseleave(function(event){
         event.preventDefault();
+        event.stopPropagation();
         if(!vm_options_win.hasClass('invisible'))
         {
-            vm_options_win.slideUp('slow');
+            vm_options_win.hide();
             vm_options_win.addClass('invisible');
         }
     })
 
-    //checkbox in selection window of vm_list_v1.html
-    $('.vm_options .select').click(function(event){
+    $('.options .select').click(function(event){
         event.preventDefault();
         var checkbox = $(this).find('.custom_checkbox');
 
@@ -35,7 +45,7 @@ $(document).ready(function(){
             checkbox.addClass('checkbox-unchecked');
 
         }
-        var cntCheckbox = $('.vm_options .checkbox-checked').length;
+        var cntCheckbox = $('.options .checkbox-checked').length;
         if (cntCheckbox == 0 ){
              $('.header .main-actions').hide();   
         } else {
@@ -47,5 +57,6 @@ $(document).ready(function(){
             }
         }
     });
+*/
    
 });
