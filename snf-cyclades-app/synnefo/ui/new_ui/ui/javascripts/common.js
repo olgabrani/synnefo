8a8f53d3a981b883993f8e3a96b40e3be8e7232d
Olga Brani <olgabrani@grnet.gr>
2013-08-29 15:29:21 +0300

    new ui: use of Magnific Popup as lightbox plugin

--- snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common.js
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/common.js
@@ -330,6 +330,25 @@ $(document).ready(function(){
         dictRemoveFile: "snf-Remove file",
     };
 
+
+    $('.main .files').magnificPopup({
+        delegate: 'a.show.image',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.data('title');
+            }
+        }
+    });
+
 })
 
 
