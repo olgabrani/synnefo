07e097e4ead56e05552ecf848234eb3eddd251d4
Athina Bekakou <athinabek@grnet.gr>
2013-09-27 12:17:02 +0300

    new ui: vm creation wizard functionality
    * improve the movement from one step to another
    * add the ability to close and reset the wizard

--- snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/vm-wizard.js
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/javascripts/vm-wizard.js
@@ -10,7 +10,7 @@ ui.wizard ={
 		console.log('set dimentions');
 		ui.wizard.relocation = $(window).width();
 		console.log(ui.wizard.relocation);
-		$('.vm-wizard-carousel').children('div').width(ui.wizard.relocation);
+		$('.vm-wizard-carousel').children('div.step').width(ui.wizard.relocation);
 	},
 
 	// sets the width and height of the carousel and its divs when the screen is resized (in PIXELS)
@@ -34,14 +34,40 @@ ui.wizard ={
 
 	move_to_step: function(prev_btn, next_btn) {
 		var speed =500;
+
+		// carousel movement when right or left arrow is pressed
+		$(document).keydown(function(e) {
+			if(e.keyCode==39) {
+				go_next();
+				return false;
+			}
+			else if(e.keyCode==37) {
+				go_prev();
+				return false;
+			}
+		});
+		
 		// when the button "next" is pressed show the next step (if there is a next step)
 		next_btn.click(function(e){
 			e.preventDefault();
+			go_next();
+		})
+
+		// when the button "previous" is pressed show the previous step (if there is a previous step)
+		prev_btn.click(function(e){
+			e.preventDefault();
+			go_prev();
+			
+		});
+
+
+		function go_next() {
+			console.log('going next!');
 			console.log('you clicked next!')
 			if(ui.wizard.current_step < ui.wizard.vm.total_step){
-				prev_btn.find('span').html('PREVIOUS');
 				ui.wizard.current_step++;
 				ui.wizard.current_position -=ui.wizard.relocation;
+				$('.vm-wizard-carousel').finish();
 				$('.vm-wizard-carousel').animate({left: ui.wizard.current_position+'px'}, speed);
 				ui.wizard.indicate_step(ui.wizard.current_step);
 				ui.wizard.set_movement_tags(ui.wizard.current_step, prev_btn, next_btn);
@@ -53,31 +79,41 @@ ui.wizard ={
 			}
 			else {
 				console.log('This is the last step.');
+				ui.wizard.close('.bottom', '#vm-wizard', '.overlay-area');
 			}
-		})
+		}
 
-		// when the button "previous" is pressed show the previous step (if there is a previous step)
-		prev_btn.click(function(e){
-			e.preventDefault();
+		function go_prev() {
 			if(ui.wizard.current_step > 1){
 				ui.wizard.current_step--;
 				ui.wizard.current_position +=ui.wizard.relocation;
+				$('.vm-wizard-carousel').finish();
 				$('.vm-wizard-carousel').animate({left: ui.wizard.current_position+'px'}, speed);
 				ui.wizard.indicate_step(ui.wizard.current_step);
 				ui.wizard.set_movement_tags(ui.wizard.current_step, prev_btn, next_btn);
+
+				if(ui.wizard.current_step == 3) {
+					setTimeout(function() { $('.vm-name').find('input').focus() }, speed/2);
+				}
 			}
 			else {
 				console.log('This is the 1st step.')
+				ui.wizard.close('.bottom', '#vm-wizard', '.overlay-area');
 			}
-		})
+		}
 	},
 
 	// sets the width and height of the steps and of the carousel (in PIXELS)
-	initialize_relocation: function(){
+	initialize_relocation: function(start_btns){
 			console.log('initialize_relocation');
-			ui.wizard.adjust_to_resized_screen();
-			ui.wizard.set_dimensions();
+			start_btns.click(function(e) {
+				e.preventDefault();	
+				ui.wizard.reset();
+				ui.wizard.adjust_to_resized_screen();
+				ui.wizard.set_dimensions();
+			})
 	},
+
 	// for the carousel index
 	indicate_step: function(step) {
 		$('.wizard .top .sub-menu[data-step]').hide();
@@ -86,19 +122,50 @@ ui.wizard ={
 		$('.nums').children().find('a:contains("'+ui.wizard.current_step+'")').parent('li').addClass('current');
 	},
 
-
+	// changes the text of next and previous buttons
 	set_movement_tags: function(step, left_btn, right_btn) {
 		if (step==1) {
 			left_btn.find('span').html('CANCEL');
+			right_btn.find('span').html('NEXT');
 		}
 		else if(step==ui.wizard.vm.total_step) {
+			left_btn.find('span').html('PREVIOUS');
 			right_btn.find('span').html('CREATE');
 		}
 		else {
 			left_btn.find('span').html('PREVIOUS');
 			right_btn.find('span').html('NEXT');
 		}
+	},
+
+	close: function(bottom_area, main_area, total_area) {
+		$(bottom_area).hide();
+		$(main_area).slideUp();
+		$(total_area).slideUp();
+	},
+
+	// manually sets elements to a initial state
+	reset: function() {
+		ui.wizard.current_step = 1;
+		ui.wizard.current_position = 0;
+		
+		$('.vm-wizard-carousel').css({left: ui.wizard.current_position+'px'});
+		$('.bottom').show();
+		$('#vm-wizard').find('.current').removeClass('current');
+		ui.wizard.indicate_step(ui.wizard.current_step);
+		$('#vm-wizard').find('.snf-checkbox-checked').addClass('snf-checkbox-unchecked').removeClass('snf-checkbox-checked');
+		$('#vm-wizard').find('.default').removeClass('snf-checkbox-unchecked').addClass('snf-checkbox-checked');
+		$('.details').hide();
+		$('.wizard .top .sub-menu ul li:first-child a').addClass('current');
+		ui.pickResources('small');
+		$('.wizard .step-2 .options li a:contains(DRBD)').addClass('current')
+		$('.vm-name input').val('');
+		$('.advanced-conf-options').hide();
+		$('.snf-color-picker').hide();
+
+
 	}
+
 }
 
 
@@ -114,9 +181,9 @@ $(document).ready(function(){
 		e.preventDefault();
 	})
 
-	ui.wizard.initialize_relocation();
+	ui.wizard.initialize_relocation(new_vm_btn);
 	ui.wizard.move_to_step(prev_btn, next_btn);
 	ui.wizard.set_movement_tags(ui.wizard.current_step, prev_btn, next_btn);
-
+	
 
 });
