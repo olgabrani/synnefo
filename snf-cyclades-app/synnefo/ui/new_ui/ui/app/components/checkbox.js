5172c014cc5c2ff10b51c5fa2b64a29a243e31c6
Athina Bekakou <athinabek@grnet.gr>
2014-03-17 14:10:44 +0200

    ember-ui: Add checkbox component (custom)

---
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/app/components/checkbox.js
@@ -0,0 +1,60 @@
+/* Use: */
+/* If the initial state of the checkbox is unchecked:  {{checkbox-custom}} */
+/* If the initial state of the checkbox is checked:  {{checkbox-custom checkboxState="preselected"}} */
+
+Snf.CheckboxCustomComponent = Ember.Component.extend({
+	tagName: 'a',
+	classNames: ['check'],
+	layoutName: 'components/btn-span',
+	spanCls: undefined,
+	checkedCls: 'snf-checkbox-checked',
+	uncheckedCls: 'snf-checkbox-unchecked',
+	didInsertElement: function() {
+		this.setInitClasses();
+	},
+	setInitClasses: function() {
+		var initialState = this.get('checkboxState');
+        console.log('given state: ', this.get('checkboxState'));
+		if(initialState === 'preselected')
+			this.check();
+		else
+			this.uncheck();
+	},
+	toggleParentLiState: function() {
+		var elState = this.get('spanCls');
+		var checkedCls = this.get('checkedCls');
+		var uncheckedCls = this.get('uncheckedCls');
+		
+		if(elState === checkedCls)
+			this.$().closest('li').addClass('selected');
+		else if(elState === uncheckedCls)
+			this.$().closest('li').removeClass('selected');
+	},
+	changeState : function() {
+		var currentState = this.get('spanCls');
+		if(currentState === this.get('checkedCls'))
+			this.uncheck();
+		else
+			this.check();
+		},
+    check : function() {
+		this.set('spanCls', this.get('checkedCls'));
+        this.toggleParentLiState();
+    },
+    uncheck : function() {
+		this.set('spanCls', this.get('uncheckedCls'));
+		this.toggleParentLiState();
+	},
+
+    reset: function() {
+		if(this.get('checkboxState') === 'preselected')
+			this.check();
+		else
+			this.uncheck();
+    },
+    click: function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		this.changeState();
+    }
+});
