39ee2121d2bbe3ec6ed3c5bd43c4f6c4422e0246
Athina Bekakou <athinabek@grnet.gr>
2014-03-17 14:06:22 +0200

    ember-ui: Add usage instructions for BtnSpan component

--- snf-cyclades-app/synnefo/ui/new_ui/ui/app/components/btn-span.js
+++ snf-cyclades-app/synnefo/ui/new_ui/ui/app/components/btn-span.js
@@ -1,3 +1,5 @@
+/* {{btn-span class="class-for-a" content="message" spanCls="class-for-span"}} */
+
 Snf.BtnSpanComponent = Ember.Component.extend({
 	tagName: 'a',
 	layoutName: 'components/btn-span',
