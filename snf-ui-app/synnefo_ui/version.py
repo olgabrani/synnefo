1c5294aca272582ff4f2dbca57f1b131ac5fd216
Olga Brani <olgabrani@grnet.gr>
2014-09-22 10:37:36 +0300

    new ui: Delete snf-ui-app files

--- snf-ui-app/synnefo_ui/version.py
+++
@@ -1,8 +0,0 @@
-
-__version__ = "0.16next_12002_d512502"
-__version_vcs_info__ = {
-    'branch': 'develop',
-    'revid': 'd512502',
-    'revno': 12002}
-__version_user_email__ = "olgabrani@grnet.gr"
-__version_user_name__ = "Olga Brani"
